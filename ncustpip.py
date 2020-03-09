b60c2827290a2050bbfc40cc2693910d08492889
Nicolas Autexier <nicolas.atx@gmx.fr>
2020-03-09 09:33:51 +0100

    add Frequency function

--- ncustpip.py
+++ ncustpip.py
@@ -1,3 +1,5 @@
+''' module for Data/Ia Dev Script -- Nicolas Autexier -- contact = nicolas.atx@gmx.fr  '''
+
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -6,6 +8,9 @@ from nltk.corpus import stopwords as STP
 from nltk.stem import WordNetLemmatizer
 
 from nltk.tokenize import word_tokenize
+
+import pandas as pd
+
 
 class  naturalCleanTxt (BaseEstimator,TransformerMixin):
     """Class for Normalise Txt cols from DataFrame, with nltk lemmatizer & stopwords"""
@@ -90,3 +95,55 @@ class  naturalSparc (BaseEstimator,TransformerMixin):
     def transform(self,X,y=None):
         return self.tfidf.transform(X.iloc[:, 0])
 
+class naturalFrequency():
+
+    @staticmethod
+    def proceed(df,col,nb_words=30):
+        """ Clean text from DataFrame, return new DataFrame with features & list of words for global analyse 
+        
+        Parameters
+        ----------
+        values : 
+
+        df = Your DataFrame
+        col = str name of column concerned
+        nb_words = by default set to 30, number of most frequent word used in one article
+        
+        Returns
+        -------
+        new dataFrame with cols cleanned and cols with most frequent words,
+        & arrays with most frequent word used on each cols, you need 2 object for return a,b = proceed(xxxx) """
+        
+        lemmatizer = WordNetLemmatizer()
+        stopwords = set(STP.words('english'))
+    
+        ########### lower cols used for analyse ###########
+    
+        for col in df :
+            df[col] = df[col].apply(lambda x : str(x).lower())
+            
+        ########### clean stop words col articles & selec most frequent words in articles ##########    
+            
+        count = 0
+        for text in df[f'{col}']:
+            filtered_sentence2 = []
+            word_list = nltk.word_tokenize(text)
+            
+            for v in word_list:
+                w = lemmatizer.lemmatize(v)
+                if w not in stopwords:
+                    if len(w) > 2 :
+                        filtered_sentence2.append(w)
+            
+            fd = nltk.FreqDist(filtered_sentence2)
+            eluo = fd.most_common(nb_words)
+            
+            filtered_sentence2 = []
+            for elem in eluo :
+                filtered_sentence2.append(elem[0])
+                
+            selected_sentence2 = ' '.join([w for w in filtered_sentence2])
+            df.loc[count,f"Selected_{col}"] = selected_sentence2
+            count +=1   
+            
+        return df, filtered_sentence2
